7aa2641d3d852c3618b2c39c2a7efde9f3f1aae8
moustafamorsy <mostafamorsyyy@gmail.com>
2022-08-04 16:40:43 +0200

    profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React from 'react';
+import img from './img.jpeg'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    fullName: "mostafa morsy",
+    bio: "iam very passionate person and hardwoker",
+    profession: "fullstack developer",
+    shows: true,
+    count:0,
+    img: img ,
+    style: {
+      App:{
+        textAlign: "center" 
+      },button: {
+        width: "100px", 
+        height: "50px",
+        backgroundImage:"linear-gradient(#e66465, #9198e5)",
+        border:"none",
+        fontWeight:"bold",
+
+        color: "white",
+            }, img:{
+        maxWidth: "150px", 
+      },profile:{
+   fontSize: "15spx",
+    maxWidth: "250px",
+    margin: "0 auto",
+    backgroundImage:"linear-gradient(#e66465, #9198e5)",
+    color:"white",
+      },
+    }
+  };
+ 
+
+  handleClick = (event) => {
+    event.preventDefault();
+    this.setState({ shows: !this.state.shows,count:0 });
+    
+  };
+  count() {
+    this.setState({count: this.state.count + 1});
+  }
+
+  componentDidMount() {
+    setInterval(() => this.count(),1000);
+  }
+  componentWillUnmount() {
+    clearInterval();
+  }
+
+
+
+  render() {
+
+    return (
+      <div className="App" style={this.state.style.App}>
+
+        <h2>second : {this.state.count}</h2>
+  <button  onClick={this.handleClick} style={this.state.style.button}>show profile</button>
+        {this.state.shows ? (
+
+          <article className="profile" style={this.state.style.profile}>
+            <h1>{this.state.fullName}</h1>
+            <p>{this.state.bio}</p>
+            <img  style={this.state.style.img}  src={img} alt="male" />
+            <h2>{this.state.profession}</h2>
+          </article>
+
+        ) : (<div></div>)}
+      </div>
+    );
+  }
 }
 
 export default App;
